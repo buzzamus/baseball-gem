ae80b0f088480b134b7fee7549e449e1089dd793
Brent Busby <brent.busby@yahoo.com>
2018-03-12 05:50:54 -0500

    complete batting average passing tests

--- lib/baseball/batting.rb
+++ lib/baseball/batting.rb
@@ -12,6 +12,14 @@ module Batting
     def batting_average
       avg = @hits.to_f / @at_bats.to_f
       player_average = avg.round(3)
+      batting_average_string = player_average.to_s.sub!("0", "")
+      if batting_average_string.length == 3
+        batting_average_string = "#{batting_average_string}0"
+      elsif batting_average_string.length == 2
+        batting_average_string = "#{batting_average_string}00"
+      else
+        return batting_average_string
+      end
       #logic here to turn average into a string, drop the 0 before decimal and make 3 decimal places
       #example: 0.25 should be returned as .250
     end
--- test/test_batters.rb
+++ test/test_batters.rb
@@ -4,7 +4,12 @@ require 'baseball'
 class TestBatters < Test::Unit::TestCase
 
   def test_batting_average
-    assert_equal 0.25, Baseball.batting_average(400, 100)
+    puts Baseball.batting_average(400, 100)
+    puts Baseball.batting_average(500, 100)
+    puts Baseball.batting_average(420, 134)
+    assert_equal ".250", Baseball.batting_average(400, 100)
+    assert_equal ".200", Baseball.batting_average(500, 100)
+    assert_equal ".319", Baseball.batting_average(420, 134)
   end
 
 end
